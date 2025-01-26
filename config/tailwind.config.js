b906060dafcb6d7f1ee50a951d4c9a7ae606e07f
samcyn <samsoniyanda@outlook.com>
2025-01-26 01:25:42 +0000

    feat: add navbar

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,22 +1,33 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
 
 module.exports = {
-  content: [
-    './public/*.html',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
-    },
-  },
-  plugins: [
-    // require('@tailwindcss/forms'),
-    // require('@tailwindcss/typography'),
-    // require('@tailwindcss/container-queries'),
-  ]
-}
+	content: [
+		'./public/*.html',
+		'./app/helpers/**/*.rb',
+		'./app/javascript/**/*.js',
+		'./app/views/**/*.{erb,haml,html,slim}',
+	],
+	theme: {
+		colors: {
+			...colors,
+			primary: {
+				DEFAULT: '#FF5A5F',
+				dark: '#FF385C',
+			},
+		},
+		extend: {
+			fontFamily: {
+				sans: [
+					'Inter var',
+					...defaultTheme.fontFamily.sans,
+				],
+			},
+		},
+	},
+	plugins: [
+		require('@tailwindcss/forms'),
+		require('@tailwindcss/typography'),
+		require('@tailwindcss/container-queries'),
+	],
+};
